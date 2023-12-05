401b63b4c8f3609be4459e42b3c2691e2a898320
jackmanson <jassonpolo@gmail.com>
2023-12-05 17:33:53 -0500

    ejemplo v-show muestra o elimina completamente el elemento del DOM

--- vistas/sign_up.php
+++ vistas/sign_up.php
@@ -3,31 +3,18 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    
-    <style>
-        .caja1{
-            width: 500px;
-        }
-        .caja1 > p{
-            
-        }
-    </style>
+    <title>v-show</title>
 
 </head>
 <body>
 
     <main id="app">
-        <input type="text" v-model="message">
+        <input type="number" v-model="message">
         <h1 v-if="message==1">El dia esta nublado</h1>
         <h1 v-else-if="message==2">El dia esta muy lluvioso</h1>
-        <h1 v-else="message==3">El dia esta muy soleado</h1>
     </main>
 
     
-
-
-
 
 
     <!-- Using Vue from CDN -->
